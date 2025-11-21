36873e7f59c36db289e49d09c2e9b73b14aeeb75
Giuseppe <galardigiuseppe11@gmail.com>
2025-11-21 13:22:47 +0100

    Added instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HPDAV-project
+
+## Instructions
+
+1. Downlaod file `https://drive.google.com/file/d/15sy1XKZ5XRUuGoQwPtXEWeD0p4oA4tnl/view?usp=sharing` and put it into `./data`
+2. Execute `setup.sh`
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 1. wget "https://drive.google.com/file/d/15sy1XKZ5XRUuGoQwPtXEWeD0p4oA4tnl/view?usp=sharing" to ./data
+# 1. unzip file and delete zip
+# 1. docker compose up --build -d
+# 1. connect and create db
